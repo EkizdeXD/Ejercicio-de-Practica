bdac123ef79252a39c6a01f1bbd1bec93059d4c1
EkizdeXD <alexisamena4@gmail.com>
2025-06-18 21:48:56 -0300

    Comit por las dudas

---
+++ TP_Practica/TP_Practica/Evento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Practica
+{
+    internal class Evento
+    {
+        public string Nombre { get; set; }
+        public string Fecha { get; set; }
+        public string Ubicación { get; set; }
+
+        public string obtener_descripción()
+        {
+            
+            return ;
+        }
+    }
+}
